2f582581005f39523ae88037be74c69391ddda65
Nathan Hitchings <nathan.hitchings@farmersinsurance.com>
2020-01-09 16:54:43 -0800

    Start fixing code in chp1.rkt

--- chp1.rkt
+++ chp1.rkt
@@ -1,55 +1,43 @@
 ;; #+OPTIONS: toc:nil
-;; #+TITLE: Assignment 1 Solutions
-;; * Exercise 1.1
-;;   Below is a sequence of expressions. What is the result printed by the
-;;   interpreter in response to each expression? Assume that the sequence is
-;;   to be evaluated in the order in which it is presented.
-;; ** Solution
-;;    #+BEGIN_SRC scheme :results output raw
-;;      10
-;;      (+ 5 3 4)
-;;      (- 9 1)
-;;      (/ 6 2)
-;;      (+ (* 2 4) (- 4 6))
-;;      (define a 3)
-;;      (define b (+ a 1))
-;;      (+ a b (* a b))
-;;      (= a b)
-;;      (if (and (> b a) (< b (* a b)))
-;;          b
-;;          a)
-;;      (cond ((= a 4) 6)
-;;            ((= b 4) (+ 6 7 a))
-;;            (else 25))
-;;      (+ 2 (if (> b a) b a))
-;;      (* (cond ((> a b) a)
-;;               ((< a b) b)
-;;               (else -1))
-;;         (+ a 1))
-;;    #+END_SRC
+;; 1.1.6
+;; Exercise 1.1: Below is a sequence of expressions. What is the result printed
+;; by the interpreter in response to each expression? Assume that the sequence
+;; is to be evaluated in the order in which it is presented.
+10
+(+ 5 3 4)
+(- 9 1)
+(/ 6 2)
+(+ (* 2 4) (- 4 6))
+(define a 3)
+(define b (+ a 1))
+(+ a b (* a b))
+(= a b)
+(if (and (> b a) (< b (* a b)))
+    b
+    a)
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25))
+(+ 2 (if (> b a) b a))
+(* (cond ((> a b) a)
+         ((< a b) b)
+         (else -1))
+   (+ a 1))
 
-;;    #+RESULTS:
-
-;; * Exercise 1.2
-;;   Translate the following expression into prefix form
-;;   \begin{equation}
-;;   \frac{5+4+(2-(3-(6+\frac{4}{5})))}{3(6-2)(2-7)}
-;;   \end{equation}
-;; ** Solution
-;;    #+BEGIN_SRC scheme
-;;      (/ (+ 5
-;;            4
-;;            (- 2
-;;               (- 3
-;;                  (+ 6
-;;                     (/ 4 5)))))
-;;         (* 3
-;;            (- 6 2)
-;;            (- 2 7)))
-;;    #+END_SRC
-
-;;    #+RESULTS:
-;;    : -37/150
+;; Exercise 1.2: Translate the following expression into prefix form
+;; 5 + 4 + (2 - (3 - (6 + 4/5)))
+;; -----------------------------
+;;     3(6 - 2)(2 - 7)
+(/ (+ 5
+      4
+      (- 2
+         (- 3
+            (+ 6
+               (/ 4 5)))))
+   (* 3
+      (- 6 2)
+      (- 2 7)))
+;; -37/150
 
 ;; * Exercise 1.3
 ;;   Define a procedure that takes three numbers as arguments and returns the
